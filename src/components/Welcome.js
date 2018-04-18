36ac747ad9e41948b7ff60f273ca96fd5e408539
EmmieO <emmiel77@hotmail.com>
2018-04-18 12:38:25 -0500

    Removed instructions for settings

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,21 +1,15 @@
 import React, {Component} from 'react';
 
-class Welcome extends Component{
-  render(){
-    return (
+const Welcome = () => (
       <div id="welcome" className="main">
-        <h1>Welcome to Chingu Tab!</h1>
-        <p>Chingu Tab will help you organize your favorite pages into categories for easy access.</p>
+        <h1>Welcome to Chingu MasterTab!</h1>
+        <p>Chingu MasterTab will help you organize your favorite pages into categories for easy access.</p>
         <ol className="ol-decimal">
           <li>First you'll need to create a category to group your pages</li>
           <li>You can have as many categories as you'd like, but every page must belong to a category</li>
           <li>When you visit a web page you'd like to add, right-click and add it to a category</li>
         </ol>
-        <p>You can customize your experience from the Settings menu.</p>
-        <p>To get started, create your first category!</p>
       </div>
     );
-  }
-}
 
 export default Welcome;
