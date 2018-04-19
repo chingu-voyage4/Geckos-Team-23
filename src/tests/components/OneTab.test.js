4fdfb42e439200dbd2f48a6e8cc89dfca29a8943
EmmieO <emmiel77@hotmail.com>
2018-04-19 09:10:42 -0500

    OneTab files removed and replaced with ChinguMasterTab files

--- src/tests/components/OneTab.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
-import OneTab from '../../components/OneTab';
-
-test('should render OneTab correctly', () => {
-  const wrapper = shallow(<OneTab />);
-  expect(toJSON(wrapper)).toMatchSnapshot();
-});
