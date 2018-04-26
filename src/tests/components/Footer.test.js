88096b2e0e92f839fb03738797f2b0a344cdc256
EmmieO <emmiel77@hotmail.com>
2018-04-26 08:41:23 -0500

    Remove React test/snapshots from CMT

--- src/tests/components/Footer.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import toJSON from 'enzyme-to-json';
-import Footer from '../../components/Footer';
-
-test('should render Footer correctly', () => {
-  const wrapper = shallow(<Footer />);
-  expect(toJSON(wrapper)).toMatchSnapshot();
-});
